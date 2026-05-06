9876d9229f6cda01268216d2fad32d38a66686ca
amsatrio <79563756+amsatrio@users.noreply.github.com>
2026-05-06 21:29:23 +0700

    update db url, update json_time

--- dto/json_time.go
+++ dto/json_time.go
@@ -5,46 +5,47 @@ import (
 	"errors"
 	"strings"
 	"time"
-
-	"github.com/goccy/go-json"
 )
+
+const timeLayout = "2006-01-02 15:04:05"
 
 type JSONTime struct {
 	time.Time
 }
 
 func (jt JSONTime) MarshalJSON() ([]byte, error) {
-	formatted := jt.Format("2006-01-02 15:04:05")
+	formatted := jt.Format(timeLayout)
 	return []byte(`"` + formatted + `"`), nil
 }
 
 func (jt *JSONTime) UnmarshalJSON(data []byte) error {
 	str := strings.Trim(string(data), `"`)
+	if str == "null" || str == "" {
+		return nil
+	}
 
-	parsedTime, err := time.Parse("2006-01-02 15:04:05", str)
+	parsedTime, err := time.Parse(timeLayout, str)
 	if err != nil {
 		return err
 	}
 
 	jt.Time = parsedTime
-
 	return nil
 }
 
 func (jt *JSONTime) Scan(value interface{}) error {
 	if value == nil {
-		*jt = JSONTime{Time: time.Time{}}
+		jt.Time = time.Time{}
 		return nil
 	}
 
-	// t, ok := value.(time.Time)
-	switch st := value.(type) {
+	switch v := value.(type) {
 	case time.Time:
-		*jt = JSONTime{Time: st}
+		jt.Time = v
 	case []byte:
-		return json.Unmarshal(st, jt)
+		return jt.UnmarshalJSON(v)
 	case string:
-		return json.Unmarshal([]byte(st), jt)
+		return jt.UnmarshalJSON([]byte(v))
 	default:
 		return errors.New("unsupported type for JSONTime")
 	}
